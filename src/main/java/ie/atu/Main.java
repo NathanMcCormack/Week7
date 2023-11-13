9716dc8ba4b593edf23bce987b252fdd72fd7f89
NathanMcCormack <G00414780@atu.ie>
2023-11-13 10:09:05 +0000

    Added a feature to deal with invalid numeric values

--- src/main/java/ie/atu/Main.java
+++ src/main/java/ie/atu/Main.java
@@ -1,23 +1,34 @@
 package ie.atu;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Please enter a number between 0 - 9: ");
-        Scanner myScan = new Scanner(System.in);
 
-        //Read user input as a string
-        String input = myScan.nextLine();
+        int x=0;
 
-        //Parse the input to an integer
-        int myNum = Integer.parseInt(input);
+        while(x<1){
+            System.out.println("\nPlease enter a whole number between 0 - 9: ");
+            Scanner myScan = new Scanner(System.in);
+            try{
+                //Read user input as a string
+                String input = myScan.nextLine();
 
-        //Check if the number is between 0 and 9
-        if (myNum >= 0 && myNum <= 9){
-            System.out.println("You entered " + myNum);
-        }
-        else{
-            System.out.println("Not a valid input");
+                //Parse the input to an integer
+                int myNum = Integer.parseInt(input);
+
+                //Check if the number is between 0 and 9
+                if (myNum >= 0 && myNum <= 9){
+                    System.out.println("You entered " + myNum);
+                    x++; //increment x on a valid input to exit the loop
+                }
+                else{
+                    System.out.println("Not a valid input");
+                }
+            }
+            catch(NumberFormatException e){ //"catches" an error in the try section and runs this code
+                System.out.println("Not a valid numeric value");
+            }
         }
     }
 }
